3642a5721b5ba8aedcb0e8171008ed48ff55e4a4
Andres <a2payema@gmail.com>
2020-03-26 22:09:11 -0400

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <welcome></welcome>
+  </div>
+</template>
+
+<script>
+import Welcome from './components/welcome' ;
+export default {
+  name: 'App',
+  components:{
+    Welcome: Welcome
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/welcome.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container-fluid">
+        <div class="mx-auto">
+            <h1>Andres Payema</h1>
+            <h2>PHP developer</h2>
+            <div>
+                <p> I´m Andres Payema web developer with almost 4 years of experi</p>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "welcome"
+    }
+</script>
+
+<style scoped>
+
+</style>
