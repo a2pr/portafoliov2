3eea2cba9e70c5ed82a48832252affd8d316b863
Andres <a2payema@gmail.com>
2020-05-10 12:00:12 -0400

    content for work and impossible

--- src/components/impossible-list.vue
+++ src/components/impossible-list.vue
@@ -1,5 +1,103 @@
 <template>
     <div>
+        <div>
+            Ultimos logros
+
+            Primer Curso de Symfony completado (16/2/2020)
+
+            Vue.js 2 Academy: Learn Vue Step by Step (22/03/2020)
+
+            Beginner Bootstrap 4: Hand code beautiful responsive websites fast(20/4/2020)
+        </div>
+        <div>
+            Habilidades
+                Hablar, escribir y leer Japones (on hold)
+                Aprender Boxeo
+                Aprender Arqueria
+                Aprender a bailar merengue
+                Aprender a bailar salsa
+                Aprender a cocteleria
+                Aprender a disparar un arma
+                Aprender Kendo
+                Estilo de hacer cafe
+            Financiero
+                Obtener un ingreso de R$ 5000
+                    Poder ahorrar R$4000
+                    Poder ahorrar R$10000
+                    Salario Bruto de 5k
+                Ganar un salario independiente de 250$
+                Finanzas sistema
+                    version 3 (desktop oriented)
+                    version 4 (mobile oriented)
+                Check family situation and see what i can do
+                    Andrea
+                    Diego
+                    Mami
+                    Papa
+
+            Viajes
+                Viajar a Espa;a
+                Viajar a USA
+                Viajar a UK
+                Viajar a Vieralves
+                Viajar a Rio de Janeiro
+                Viajar a Japon
+                Hokkaido
+                    Tokyo
+                    Kyoto
+                Viajar solo cualquier destino
+                Viajar a Venezuela
+            Eventos que asistir
+                The international
+                Un mayor de dota 2
+                Comic con
+                Comiket
+            Programacion
+                Herramientas del trabajo
+                Crear mi propio Blog
+                    [Blog de cafes](https://www.notion.so/Blog-de-cafes-5cad7fceac664c8a88e58b7aa6bf01fe)
+
+                Crear una pagina web
+                Set up automatic emails
+                Modificar my portafolio online
+                Crear una aplicacion movil
+                Crear un bot de discord
+                Crear una extension de google chrome for manga tracking
+                Aprender Mysql
+                Aprender Node.js
+                Restful API
+                Aprender JQuery
+                Aprender React.js
+                Redis
+                Aprender Symfony
+                    Primer Curso de Symfony completado (16/2/2020)
+                    Segundo Curso de Symfony
+                Realizar 5 projectos en Symfony
+                    Escribir problemas y beneficios de Symfony
+                Aprender Doctrine
+                Aprender Vue.js
+                    Vue.js 2 Academy: Learn Vue Step by Step
+                    Vue.js: Build a Full Stack App With Firebase, Vuex & Vue Router
+                Aprender Docker
+                    set first docker with all the repos i got
+                    Set a docker with my portfolio environment.
+                Maintain knowledge in Bootstrap
+                    Beginner Bootstrap 4: Hand code beautiful responsive websites fast
+            Materiales
+                Tablet para ver anime
+                5 camisas personalizadas de anime
+                funda para LN
+                XBOX One
+
+                [Lista de necesidades](https://www.notion.so/Lista-de-necesidades-17e0a983d7074e2ca0a4e75fb4c3fa4a)
+
+                Una laptop de trabajo
+
+            Fisico
+                Reducir peso a 75~80 kg
+                Mantener una rutina semanal de 15 min de ejercicio( 2 semanas)
+        </div>
+
 
     </div>
 </template>
--- src/components/work.vue
+++ src/components/work.vue
@@ -1,31 +1,77 @@
 <template>
-    <div>
-        this is works done
-        * testing testing testing
-            As part of Made simple group, i have being writting/developing tests to verify new features launch at:
-                companiesmadesimple.com
-        * Modify Rss reader
-            A simple rss reader i modify to get the contents from new post from reddit and mangadex
+    <div class="text-center">
+        <div class="container-fluid py-5  works" v-for="(work, index) in works" :key="index">
+            <div class="col-6 mx-auto">
+                <img v-if="work.img" :src='work.img' alt=""/>
+                <h1> {{work.title}}</h1>
+                <p>{{work.description}}</p>
+                <a class="btn btn-outline-dark" :href="work.link">
+                    <i class="fa fa-github"></i>
+                </a>
+            </div>
 
-        * Music collection app
-            A simple music library with login using MVC framework. That list users preferend artists, albums an musics.
 
-        * FinatialTracking App
-            Currently working in an app to store daily income and expenses, using dinamic funds and subfunds that the user can modify depending of need.
-            Using graphics to show user expending behavior and alarms to notify self-proclaimed limit reach in each sub-fund created.
+        </div>
 
-        * Pizza management app
-            App to manage pizzeria management routines and adding new orders from client, using login for users and admin.
-            https://github.com/a2pr/Programa-PIM
     </div>
 </template>
 
 <script>
     export default {
-        name: "work"
+        name: "work",
+        data()
+        {
+            return {
+                works: {
+                    1: {
+                        'title': 'Testing testing testing',
+                        'description': ' As part of Made simple group, i have being writting/developing tests to verify new features',
+                        'img': '',
+                        'link': 'https://www.companiesmadesimple.com/'
+                    },
+                    2: {
+                        'title': 'Music collection app',
+                        'description': 'A simple music library with login using MVC framework. That list users preferend artists, albums an musics.',
+                        'img': '',
+                        'link': 'https://github.com/a2pr/musiccollection'
+                    },
+                    3: {
+                        'title': 'Modify Rss reader',
+                        'description': 'A simple rss reader i modify to get the contents from new post from reddit and mangadex',
+                        'img': '',
+                        'link': 'https://github.com/a2pr/rss-personal-reader'
+                    },
+                    4: {
+                        'title': 'FinatialTracking App',
+                        'description': 'Currently working in an app to store daily income and expenses, using dinamic funds and subfunds ' +
+                            'that the user can modify depending of need.\n' +
+                            'Using graphics to show user expending behavior and alarms to notify self-proclaimed limit reach in each sub-fund created.',
+                        'img': '',
+                        'link': 'https://github.com/a2pr/fta-project'
+                    },
+                    5: {
+                        'title': 'Pizza management app',
+                        'description': 'App to manage pizzeria management routines and adding new orders from client, using login for users and admin.',
+                        'img': '',
+                        'link': 'https://github.com/a2pr/Programa-PIM'
+                    }
+                }
+            }
+        }
     }
 </script>
 
 <style scoped>
 
+    .works:nth-child(even){
+        background: #CCC;
+    }
+    .works:nth-child(odd){
+        background: #FFF;
+    }
+
+
+    i{
+        font-size: 40px;
+    }
 </style>
